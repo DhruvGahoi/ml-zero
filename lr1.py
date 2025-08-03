513909b241e6453847e6fde4b763299a42057f37
DhruvGahoi <dhruvgahoi11111@gmail.com>
2025-08-03 11:59:17 +0530

    First LR model : challenges

--- lr1.py
+++ lr1.py
@@ -3,26 +3,31 @@ import matplotlib . pyplot as plt
 
 # Here we are making a basic Linear Regression Model
 
-m = 0.5
-b = 20
+# m = 0.5
+# b = 20
 
+#Data
 X = np.array([2, 3, 5, 6, 8])
 y_true = np.array([65, 70, 75, 85, 90])
 
+#Predict function
 def predict(X, m, b):
     # y = mx + b equation
     return m * X + b
 
-y_pred = predict(X, m, b)
-print(y_pred)
+# y_pred = predict(X, m, b)
+# print(y_pred)
 
+#Loss Function(Mean Squared Error)
 def loss(y_true, y_pred):
     return np.mean((y_true - y_pred)**2)
 
-init_loss = loss(y_true, y_pred)
-print(f"Initial Loss : {init_loss}")
+# init_loss = loss(y_true, y_pred)
+# print(f"Initial Loss : {init_loss}")
 
 learning_rate = 0.01
+
+#Gradient descent update
 def update(X, y_true, y_pred, m, b, learning_rate):
     dm = -2 * np.mean(X * (y_true - y_pred))
     db = -2 * np.mean(y_true - y_pred)
@@ -32,21 +37,39 @@ def update(X, y_true, y_pred, m, b, learning_rate):
     return new_m, new_b
 
 # Reinitialized variables 
-m = 0.0
-b = 0.0
+#m = 0.0
+#b = 0.0
 epochs = 2000
 learning_rate = 0.01
 
+    #def train_model(X, y_true, learning_rate, epochs=1000):
+m = 0.0
+b = 0.0
 loss_history = []
 for i in range(epochs):
     y_pred = predict(X, m, b)
     curr_loss = loss(y_true, y_pred)
     loss_history.append(curr_loss)
-
     if i % 100 == 0:
         print(f"Epoch {i}: Loss = {curr_loss:.2f}, m = {m:.2f}, b = {b:.2f}")
 
     m, b = update(X, y_true, y_pred, m, b, learning_rate)
+
+#    return loss_history, m, b
+
+# learning_rates = {
+        #    "Good (0.01)" : 0.01, 
+        #"Too Low (0.0001)": 0.0001,
+        #    "Too High (0.1)": 0.1
+# }
+ 
+# plt.figure(figsize=(10, 6))
+# for label, lr in learning_rates.items():
+# loss_history, m, b = train_model(X, y_true, learning_rate=lr)
+    #   print(f"Final Loss ({label}): {loss_history[-1]:.2f}")
+    # print(f"Optimal parameters ({label}): m = {m:.2f}, b = {b:.2f}\n")
+#    plt.plot(loss_history, label=f"{label}")
+
 
 print("\n --- Training Complete ---")
 print(f"Final Loss: {loss_history[-1]:.2f}")
@@ -54,7 +77,10 @@ print(f"Optimal parameters: m = {m:.2f}, b = {b:.2f}")
 
 
 plt.plot(loss_history)
-plt.title("Loss Curve during Training")
+plt.title("Loss Curve for Different Learning Rates")
 plt.xlabel("Epoch")
-plt.ylabel("Mean Squared Error")
+plt.ylabel("Loss")
+# plt.legend()
+# plt.grid(True)
+# plt.tight_layout()
 plt.show()
